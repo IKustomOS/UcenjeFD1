f92fd7dc36bb444d658bd7e43687a48879d5ca9b
IKustomOS <kaniza.ivan@gmail.com>
2026-01-27 18:49:43 +0100

    Javascript Hello world!

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,15 @@
+console.info("Hello world! 5")
+console.table(JSON.parse('{"name":"John", "age":30, "car":null}'))
+
+console.log("O'Neil");
+console.log('O\'Neil');
+
+console.log(`Hello
+    World
+    6
+    `);
+
+// Backtick string with variable interpolation, Copilot suggestion
+console.log(`This is a backtick string with a variable: ${5 + 3}`);
+
+document.getElementById('ispis').innerHTML = '<a href="#">Poveznica</a>';
